59dc964eb6d759c36b92d0ca8e516ab974414800
Remolin <jhoner@bssinternal.com>
2020-02-28 05:14:35 +0800

    git remote add origin https://github.com/jhonerApp/DatingApp_v1.git

--- DatinApp.API/Controllers/AuthController.cs
+++ DatinApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatinApp.API.Data;
 using DatinApp.API.Dtos;
 using DatinApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatinApp.API.Controllers
 {
@@ -11,11 +17,16 @@ namespace DatinApp.API.Controllers
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
+        private readonly IConfiguration _config;
+
         public IAuthRepository _repo { get; }
-        public AuthController(IAuthRepository repo)
+        public AuthController(IAuthRepository _repo, IConfiguration config)
         {
-            this._repo = repo;
+            this._config = config;
+            this._repo = _repo;
+
         }
+
 
 
         [HttpPost("register")]
@@ -36,6 +47,45 @@ namespace DatinApp.API.Controllers
 
             return StatusCode(201);
 
+        }
+
+        [HttpPost("login")]
+
+        public async Task<IActionResult> Login(UserForRegisterDto UserForLoginDto)
+        {
+
+            var userFromRepo = await _repo.Login(UserForLoginDto.Username.ToLower(), UserForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claim = new[]
+            {
+                   new Claim(ClaimTypes.NameIdentifier ,userFromRepo.Id.ToString()),
+                   new Claim(ClaimTypes.Name ,userFromRepo.Username)
+
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+
+                Subject = new ClaimsIdentity(claim),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new{
+
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
